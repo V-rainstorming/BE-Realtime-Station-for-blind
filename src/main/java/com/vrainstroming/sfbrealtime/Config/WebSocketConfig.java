dfb1d78151528283a2d14f703ba5bd3fd78a05ee
SeungBeomHu <hsb0423@naver.com>
2024-05-23 16:53:58 +0900

    feat : 버스 위치 정보 실시간 저장 서비스 구현
    - 소켓 설정파일 추가

--- src/main/java/com/vrainstroming/sfbrealtime/Config/WebSocketConfig.java
+++ src/main/java/com/vrainstroming/sfbrealtime/Config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.vrainstroming.sfbrealtime.Config;
 
+import com.vrainstroming.sfbrealtime.Service.BusPosUpdateHandler;
 import com.vrainstroming.sfbrealtime.Service.MyWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -10,13 +11,20 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final MyWebSocketHandler myWebSocketHandler;
+    private final BusPosUpdateHandler busPosUpdateHandler;
 
-    public WebSocketConfig(MyWebSocketHandler myWebSocketHandler) {
+    public WebSocketConfig(MyWebSocketHandler myWebSocketHandler,BusPosUpdateHandler busPosUpdateHandler) {
         this.myWebSocketHandler = myWebSocketHandler;
+        this.busPosUpdateHandler = busPosUpdateHandler;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myWebSocketHandler, "/busPosUpdateDemo").setAllowedOrigins("*");
         registry.addHandler(myWebSocketHandler, "/busPosUpdate").setAllowedOrigins("*");
+
+
+
+
     }
 }
---
+++ src/main/java/com/vrainstroming/sfbrealtime/Service/BusPosUpdateHandler.java
@@ -0,0 +1,43 @@
+package com.vrainstroming.sfbrealtime.Service;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vrainstroming.sfbrealtime.mapper.BusPosMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Map;
+
+
+@Component
+@Slf4j
+public class BusPosUpdateHandler extends TextWebSocketHandler {
+
+    @Autowired
+    BusPosMapper busPosMapper;
+
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+
+        String payload = message.getPayload();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            Map<String,Object> dto = objectMapper.readValue(payload,Map.class);
+            busPosMapper.updateTempDemoBusPos(dto);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
